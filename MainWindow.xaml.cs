8c5ea71b0545e53d3f6ecc5c4975700c927a09f7
Pengo Wray <peter@becauseofgames.com>
2022-09-20 09:34:06 +1000

    Bleeps on button right-press

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MorseKeyer.Sound;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MorseKeyer {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        Sounder Sounder;
+
+        public MainWindow() {
+            InitializeComponent();
+            Sounder = new Sounder();
+            Sounder.Enable();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            //todo: delete me
+            //Sounder.StraightKeyDown();
+        }
+
+        private void Button_MouseDown(object sender, MouseButtonEventArgs e) {
+            Sounder.StraightKeyDown();
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e) {
+            Sounder.StraightKeyUp();
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e) {
+            // stop it getting stuck
+            Sounder.StraightKeyUp();
+        }
+    }
+}
---
+++ Sound/Sounder.cs
@@ -0,0 +1,90 @@
+﻿using NAudio.Wave.SampleProviders;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MorseKeyer.Sound
+{
+    public class Sounder {
+
+        private int Latency = 50;
+        IWavePlayer waveOutDevice;
+        SignalGenerator Sine;
+
+        //WaveStream mainOutputStream;
+
+
+
+        public Sounder(int latency = 50) {
+            Latency = latency;
+
+            Sine = new SignalGenerator() {
+                Gain = 0.4,
+                Frequency = 550,
+                Type = SignalGeneratorType.Sin
+            };
+        }
+
+        public void Enable() {
+            // start running.
+            try {
+                waveOutDevice = new DirectSoundOut(Latency);
+                
+                
+            } catch (Exception driverCreateException) {
+                Console.WriteLine(String.Format("{0}", driverCreateException.Message));
+                return;
+            }
+
+            //TODO: sounds
+            /*
+            mainOutputStream = CreateInputStream(fileName);
+            try {
+                waveOutDevice.Init(mainOutputStream);
+            } catch (Exception initException) {
+                Console.WriteLine(String.Format("{0}", initException.Message), "Error Initializing Output");
+                return;
+            }
+            */
+
+        }
+
+        public void DitKeyDown() {
+            //mainOutput
+        }
+
+        public void StraightKeyDown() {
+            // https://github.com/naudio/NAudio/blob/master/Docs/PlaySineWave.md
+            // https://csharp.hotexamples.com/examples/NAudio.Wave/DirectSoundOut/Play/php-directsoundout-play-method-examples.html
+
+
+            //Sine.Take(TimeSpan.FromSeconds(20));
+            //using (var wo = new WaveOutEvent()) {
+            //    wo.Init(sine.Take(TimeSpan.FromSeconds(1)));
+            //    wo.Play();
+            //    while (wo.PlaybackState == PlaybackState.Playing) {
+            //        Thread.Sleep(500);
+            //    }
+            //}
+
+            waveOutDevice.Init(Sine);
+            //waveOutDevice.Init(Sine.Take(TimeSpan.FromSeconds(1)));
+            waveOutDevice.Play();
+            Console.WriteLine("playing...");
+
+        }
+
+        public void StraightKeyUp() {
+            if (waveOutDevice?.PlaybackState == PlaybackState.Playing) {
+                waveOutDevice.Pause();
+            }
+        }
+
+
+
+    }
+}
